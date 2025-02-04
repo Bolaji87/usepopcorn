93855293b322d2679a43d048f2f0891ba547413f
Bolaji87 <sakabolaji77@gmail.com>
2025-02-04 16:14:44 +0100

    feat: receive prop

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,32 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import "./index.css";
 // import App from "./App.jsx";
 import StarRating from "./components/StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        color="blue"
+        maxRating={10}
+        size={40}
+        onSetRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "okay", "good", "amazing"]}
+    />
+    <StarRating maxRating={5} color="red" size={24} defaultRating={3} />
+
+    <Test />
   </StrictMode>
 );
